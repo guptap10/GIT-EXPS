599dd9c3d3bfe6295a594a1f7b9d3699b1e43a14
Prakhar Gupta <prakhargupta.1092@gmail.com>
2024-08-30 20:13:09 +0530

    Add files via upload

---
+++ exp9/exp9_withoutptr.cpp
@@ -0,0 +1,17 @@
+ //PRAKHAR GUPTA
+ //23070123101
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *ptr;
+    int a[5] = { 1,2,4,8,9};
+    ptr = &a[0];
+    int i;
+    for(i=0 ; i<5 ; i++)
+    {
+        cout << "Element "<< i+1 <<" "<<"="<<" "<<*(a+i) << endl;
+        ptr ++;
+
+    }
+}
