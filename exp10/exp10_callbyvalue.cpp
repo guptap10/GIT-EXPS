fef73904908d4d9d9e43f37bbd38f85b4b6a7531
Prakhar Gupta <prakhargupta.1092@gmail.com>
2024-08-30 22:04:35 +0530

    Add files via upload

---
+++ exp10/exp10_callbyvalue.cpp
@@ -0,0 +1,20 @@
+//PRAKHAR GUPTA
+//23070123101
+ #include <iostream>
+using namespace std;
+ void swap(int a,int b)
+ 
+{
+    int c;
+    c=a;
+    a=b;
+    b=c;
+   cout<<"Inside swabByValue function: "<<"a = "<<a<<" , "<<"b = "<<b<<endl;
+}
+int main()
+ 
+{ int a=2,b=7;
+cout<<"Before swabByValue: "<<"a = "<<a<<" , "<<"b = "<<b<<endl;
+swap(a,b);
+cout<<"After swapByValue: "<<"a = "<<a<<" , "<<"b = "<<b<<endl;
+}
