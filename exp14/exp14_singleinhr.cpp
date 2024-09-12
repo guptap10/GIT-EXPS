999491ef7c7ac2b96f447460dbb99b60511091ad
Prakhar Gupta <prakhargupta.1092@gmail.com>
2024-09-12 17:50:09 +0530

    Add files via upload

---
+++ exp14/exp14_singleinhr.cpp
@@ -0,0 +1,44 @@
+//PRAKHAR GUPTA
+//23070123101
+#include <iostream>
+using namespace std;
+
+class Bands {
+
+public :
+string bd;
+string sr;
+void genre(){
+
+    cout<<"Enter the name of band: "<<endl;
+    cin>>bd>>sr;
+    
+
+}
+};
+
+class Lz : public
+Bands {
+    public:
+     int est;
+    void estd(){
+       
+    cout<<"Estalished: "<<endl;
+    cin>>est;
+    
+}
+void display(){
+    cout<< "The band is: "<<bd<<" "<<sr<<endl;
+    cout<<"Established: "<<est<<endl;
+
+}
+};
+int main()
+{
+    Lz d;
+    d.genre();
+    d.estd();
+    d.display();
+    
+    return 0;
+}
