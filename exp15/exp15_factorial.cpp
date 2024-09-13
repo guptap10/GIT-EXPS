4b6c7b6bebb9f61bea34686f71e82e5b22c5d9bd
Prakhar Gupta <prakhargupta.1092@gmail.com>
2024-09-13 10:36:41 +0530

    Add files via upload

---
+++ exp15/exp15_factorial.cpp
@@ -0,0 +1,22 @@
+//PRAKHAR GUPTA
+//23070123101
+# include<iostream>
+using namespace std;
+ int factorial (int n)
+{ if ( n == 0)
+{
+    return 1;
+}
+else 
+{
+    return n*factorial(n-1);
+    }
+    }
+    int main()
+    {  int num;
+       cout<<"Enter a number: "<<endl;
+       cin>>num;
+
+       cout<<"The factorial of "<<num<<" is: "<< factorial(num)<<endl;
+       return 0;
+    }
