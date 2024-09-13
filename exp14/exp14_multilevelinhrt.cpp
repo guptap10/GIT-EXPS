48fad26a344b101c3eee6363bf208b8ebfa40483
Prakhar Gupta <prakhargupta.1092@gmail.com>
2024-09-13 10:16:57 +0530

    Add files via upload

---
+++ exp14/exp14_multilevelinhrt.cpp
@@ -0,0 +1,51 @@
+# include<iostream>
+using namespace std;
+
+class planets {
+    public:
+    string st, ju;
+
+    void gas(){
+        cout<< "The planet which are gas giants: "<<endl;
+        cin>>st>>ju;
+        
+    }
+};
+class li :
+public
+planets{
+    public:
+    string et;
+    void life(){
+    
+    cout<< " Planet with life: "<<endl;
+    cin>>et;
+    
+}
+};
+
+class SolarSystem : public
+li{
+    public:
+    string cm,cl;
+    void comets(){
+        cout<< "The home of comets: "<<endl;
+        cin>>cm>>cl;
+        
+    }
+
+
+void display(){
+     cout<< st<<" & "<<ju << " are gas giants."<<endl;
+  
+     cout<< et <<" is the planet with life."<<endl;
+  cout<< cm <<" "<< cl<<" is the home of comets"<<endl;
+}
+};
+int main (){
+    SolarSystem d;
+    d.life();
+    d.comets();
+    d.gas();
+    d.display();
+}
