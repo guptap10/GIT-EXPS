3977db648def7d24ae2d7cd5b73f6404d3614cc1
Prakhar Gupta <prakhargupta.1092@gmail.com>
2024-09-27 10:33:28 +0530

    Add files via upload

---
+++ exp16/exp16_posneg.cpp
@@ -0,0 +1,23 @@
+//PRAKHAR GUPTA
+//23070123101
+# include<iostream>
+using namespace std;
+ int main()
+ {  float a;
+    cout<< "Enter a positive  numbers: "<<endl;
+    cin>>a;
+
+try {
+    if ( a<0)
+    {
+        throw a;
+    }
+else {
+ cout<< "The number  is: "<<a<<endl;
+ }
+}
+catch (const float n)
+{
+    cout<<"You entered " <<a<<" which is a negative number"<<n;
+}
+ }
---
+++ exp16/exp16_year.cpp
@@ -0,0 +1,27 @@
+//PRAKHAR GUPTA
+//23070123101
+# include<iostream>
+using namespace std;
+ int main()
+ { int year;
+    cout << "Enter year greater than 2000: "<<endl;
+    cin>>year;
+
+    try{
+if ( year<2000)
+{
+    throw "You entered a year less than 2000";
+
+}
+else{
+    cout<< "Entered year is: "<<year<<endl;
+}
+    }
+
+    catch ( const char*msg)
+
+    { 
+        cout << msg;
+        
+    }
+ }
