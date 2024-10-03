1fb23f7d3537bc5506de08ccc8ae43d815de0ec9
Prakhar Gupta <prakhargupta.1092@gmail.com>
2024-10-03 15:28:34 +0530

    Add files via upload

---
+++ exp17/exp17_node.cpp
@@ -0,0 +1,18 @@
+//PRAKHAR GUPTA
+//23070123101
+#include<iostream>
+using namespace std;
+class Link{
+    public:
+    int data ;
+    Link* next;
+    Link(int num){
+        data = num;
+        next = NULL;
+    }
+};
+int main(){
+    Link* l1 = new Link(6);
+    cout << l1->data<<"   "<<l1->next;
+
+}
